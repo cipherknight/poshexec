83eff08bba3c8e2d0d2eeb9fc632e78c39a80dc0
cipherknight <giuseppe.sammarco@gmail.com>
2023-08-28 02:06:47 +0200

    First commit

---
+++ poshexec.go
@@ -0,0 +1,31 @@
+package poshexec
+
+import (
+	"bytes"
+	"os/exec"
+)
+
+type PowerShell struct {
+	powerShell string
+}
+
+func New() *PowerShell {
+	ps, _ := exec.LookPath("powershell.exe")
+	return &PowerShell{
+		powerShell: ps,
+	}
+}
+
+func (p *PowerShell) Execute(args ...string) (stdOut string, stdErr string, err error) {
+	args = append([]string{"-NoProfile", "-NonInteractive"}, args...)
+	cmd := exec.Command(p.powerShell, args...)
+
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err = cmd.Run()
+	stdOut, stdErr = stdout.String(), stderr.String()
+	return
+}
